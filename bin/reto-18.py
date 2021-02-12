a109974284b6fadb576fe31de9f2ad1c22c56adb
Hugo Aquino <hugo.aquino.navarrete@gmail.com>
2021-02-12 12:15:08 -0500

    Ajuste a reto 18 así como a presentación para registrar cambios del reto antes mencionado

--- bin/reto-18.py
+++ bin/reto-18.py
@@ -1,8 +1,8 @@
 # Reto 18
 
-# En el directorio "input" está un archivo que se llama "libras.txt".
+# En el directorio "input" está un archivo que se llama "mlb_teams.csv".
 # Crea en Python un directorio a partir de esta información donde se contabilice el número de veces
-# que aparece el dato en cuestión
+# que aparece el año (primer campo de cada línea) para saber cuantos equipos había por año.
 
 # A partir de esta línea indica como resolverías el reto.
 
